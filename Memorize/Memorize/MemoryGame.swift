f015d4634cb3800d4f68f1321471e3842e41d389
camotsuc <57636182+camotsuc@users.noreply.github.com>
2020-07-04 11:11:32 +0300

    Update MemoryGame.swift

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -14,6 +14,7 @@ struct MemoryGame<CardContent> {
             cards.append(Card(content: content, id: pairIndex*2))
             cards.append(Card(content: content, id: pairIndex*2+1))
         }
+        cards.shuffle()
     }
     
     struct Card: Identifiable {
